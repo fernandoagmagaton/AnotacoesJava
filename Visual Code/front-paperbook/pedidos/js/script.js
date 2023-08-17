aee900199a214f3497c1d58fc73dcf4660e8421d
Fernando Magaton <104094324+fernandoagmagaton@users.noreply.github.com>
2023-08-17 17:15:38 -0300

    Add files via upload

---
+++ Visual Code/front-paperbook/pedidos/js/script.js
@@ -0,0 +1,21 @@
+function carregarDados(){
+
+    let lstpedidos = document.getElementById("lstPedidos");
+    let dados = "";
+
+    fetch("http://10.26.45.40:5000/api/v1/pedido/listar")
+    .then((response) => response.json())
+    .then((rs)=>{
+        console.log(rs);
+        rs.map((itens, ix)=>{
+            dados += `<ul id="itempedido">
+                        <li>${itens.idpedido}</li>
+                        <li>${itens.loja}</li>
+                        <li>${itens.unidadeloja}</li>
+                        <li>${itens.numeroprotocolo}</li>
+                        <li>${itens.valortotalpedido}</li>
+                    </ul> `;
+                    lstpedidos.innerHTML +=dados;
+        })
+    }).catch((err)=>console.error(err))
+}
